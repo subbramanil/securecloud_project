e73bdcc633c4b8f683de3e730f7e4d4ad73ade13
Subbramanian Lakshmanan <subbu@esi-estech.com>
2016-04-28 11:26:48 -0500

    ajax request for login

--- WebUI/public/login/login.js
+++ WebUI/public/login/login.js
@@ -1,7 +1,6 @@
 'use strict';
 
 angular.module('login', ['ngRoute'])
-
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/login', {
             templateUrl: 'public/login/login.html',
@@ -9,34 +8,60 @@ angular.module('login', ['ngRoute'])
         });
     }])
 
-    .controller('LoginCtrl', ['$scope', 'LoginProp', function ($scope, LoginProp) {
+    .controller('LoginCtrl', ['$scope', 'LoginService', function ($scope, loginService) {
         $scope.userName = "";
         $scope.password = "";
         $scope.login = function () {
             console.log("Login() entry");
             console.log($scope.userName, $scope.password);
+            loginService.login($scope.userName, $scope.password);
             console.log("Login() exit");
         };
 
     }])
 
-    .service('LoginProp', function () {
-        var Items = '';
-        var Total = 0;
+    .service('LoginService', [
+        '$http',
+        '$q',
+        function ($http, $q) {
 
-        return {
-            getItems: function () {
-                return Items;
-            },
-            setItems: function (value) {
-                Items = value;
-            },
-            getTotal: function () {
-                return Total;
-            },
-            setTotal: function (value) {
-                Total = value;
-            }
-        };
-    });
+            var Items = '';
+            var Total = 0;
+
+            return {
+
+                login: function (username, passwd) {
+                    console.log("LoginService.login() entry");
+                    var params = "?userName=" + username + "&password=" + passwd;
+                    var defer = $q.defer();
+                    $http(
+                        {
+                            method: "get",
+                            url: "http://localhost:8080/demoREST/rest/authenticate" + params
+                        }
+                    ).then(function (response) {
+                            console.log("Response: ", response.data);
+                            defer.resolve(response);
+                        },
+                        function (error) {
+                            console.log("Error: ", error);
+                            defer.resolve(error);
+                        });
+                    console.log("LoginService.login() exit");
+                    return defer.promise;
+                },
+                getItems: function () {
+                    return Items;
+                },
+                setItems: function (value) {
+                    Items = value;
+                },
+                getTotal: function () {
+                    return Total;
+                },
+                setTotal: function (value) {
+                    Total = value;
+                }
+            };
+        }]);
 
